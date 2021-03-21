c1192e8d5e4354d041c36655673adc758d2d6dea
Maycol Michel Mohr <maycolmohr@yahoo.com.br>
2021-03-21 20:50:51 +0100

    Good solutiongit add .!

---
+++ src/main/java/model/exceptions/DomainException.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.exceptions;
+
+/**
+ *
+ * @author marin
+ */
+public class DomainException extends RuntimeException {
+    
+    
+public DomainException(String msg) {
+    super(msg);
+}    
+    
+}
